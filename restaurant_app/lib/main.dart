dca0d96ce9f6a5999cd25c8d67c3e281239605e4
sabato <sabato@hura.one>
2022-01-04 00:42:26 +0200

    commit

--- restaurant_app/lib/main.dart
+++ restaurant_app/lib/main.dart
@@ -1,20 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/screens/login_screen.dart';
+import 'package:food_ordering_app/views/HomePage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // Base Widget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Login',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: LoginScreen());
+      title: 'Kigali Food Explorer',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
